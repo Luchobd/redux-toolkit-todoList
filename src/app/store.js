3a7878f7f9e1324fd4d21be499a7d5f7d712c5c5
Luis Blanco <luiswblancod@gmail.com>
2022-08-27 17:45:19 -0500

    todo list redux toolkit

---
+++ src/app/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import taskSliceReducer from "../features/tasks/taskSlice";
+
+export const store = configureStore({
+  reducer: {
+    tasks: taskSliceReducer,
+  },
+});
